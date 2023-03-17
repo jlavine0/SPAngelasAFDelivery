e8d592aa30a328ee3e14aab72cde40c836d26d0e
jlavine0 <jdl05160@vsc.edu>
2023-03-16 21:25:40 -0400

    First Linkages Java to XML
    Main, drink, food, and pickup menus have button id linkages between Java and XML code. Problem: 'toolbar' and 'fab' Java binding symbols seem to be broken, suspected cause: change in layout (coordinator -> constraint), will revisit.

--- AAFDApp/app/src/main/java/com/example/aafdapp/MainActivity.java
+++ AAFDApp/app/src/main/java/com/example/aafdapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.aafdapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void food(View view)
+    {
+
+    }
+
+
+    public void drink(View view)
+    {
+
+    }
 }
